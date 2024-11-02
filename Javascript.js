4d6898e283ecef4c4c4a8afc9c5141a677ab0407
Averson01 <167694823+Averson01@users.noreply.github.com>
2024-11-02 15:38:31 +0000

    nova actualizacao

--- Javascript.js
+++ Javascript.js
@@ -5,4 +5,6 @@ const calcular = document.getElementById("calcular")
 const resultado = document.getElementById("resultado")
  /* aqui estamos a dizer para pegar o elemento cujo o id é ... */
 
- 
+ nome.value = " Áverson Wambembe" /* estamos atribuindo na propriedade nome, o valor Averson */
+
+ /* o ponto(.) é para acessar a propriedade dele */
